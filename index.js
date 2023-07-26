879c88d64f02c1a4eb50c3702a3309c9981a13d6
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-07-26 19:59:48 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var employee = {name:"abdou",streetAddress:"cite 72 logts"};
+var updateEmployeeWithKeyAndValue=function(obj,key,value){
+    // obj={...employee};
+   // obj[key]=value;
+  return {...employee,
+    [key]:value,}
+}
+
+var destructivelyUpdateEmployeeWithKeyAndValue=function(obj,key,value){
+employee[key]=value
+return employee;
+}
+
+const deleteFromEmployeeByKey=function(employee,key){
+     // return {...employee,
+    //"key":"value",}
+     
+         var obj={...employee};
+          return obj[key]={};
+          console.log (obj)
+
+}
+const destructivelyDeleteFromEmployeeByKey=function(employee,key){
+  delete employee[key]
+  return employee
+}
